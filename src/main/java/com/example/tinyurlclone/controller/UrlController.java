58fc853d8edba8ed48583f1926aa24e66e1d6800
Pranav Shukla <pranavshukla@Pranavs-MacBook-Air.local>
2024-07-28 14:18:15 +0530

    enabling cors

--- src/main/java/com/example/tinyurlclone/controller/UrlController.java
+++ src/main/java/com/example/tinyurlclone/controller/UrlController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:49273")
+@CrossOrigin(origins = "https://tinyurlclone.onrender.com/")
 public class UrlController {
 
     @Autowired
